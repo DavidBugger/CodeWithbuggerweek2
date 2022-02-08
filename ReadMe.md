8cced93fb8176163bb7a54eabbb1d73e204673f6
DavidBugger <bwehfukdavid@gmail.com>
2022-02-08 09:34:31 +0100

    Web development

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+CodeWithBugger 1.0 is a digital skills on web development both frontend and backend stacks 
+The languages are HTML, CSS, and javascript 
+You can send me a message on Whatsapp @+2348107701730
+Or Email: buggershub@gamil.com
---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("nav01").innerHTML =
+"<ul id = 'menu'>"+
+"<li><a href = 'index.html'>Home</a></li>"+
+"<li><a href='participants.html'>Participants</a></li>"+
+"<li><a href ='login.html'>Login</a></li>";
+
+// footer 
+document.getElementById("foot01").innerHTML =
+"<p>&copy "+ new Date().getFullYear() + "CodeWithBugger 1.0 </p>";
